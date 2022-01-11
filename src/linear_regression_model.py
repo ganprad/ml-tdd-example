ef0f49fe7c68767c4c2ee3ecaa4f818dbd14053c
pganesan <pradeep.ganesan@objectpartners.com>
2022-01-11 15:22:26 -0600

    Added linear regression model skeleton

---
+++ src/linear_regression_model.py
@@ -0,0 +1,25 @@
+from src.basewrappermodel import BaseWrapperModel
+
+
+class LinearRegressionModel(BaseWrapperModel):
+    """Skeleton for baseline scikit-learn Linear Model interface"""
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def tune(self):
+        pass
+
+    def predict_proba(self):
+        pass
+
+    def preprocess(self):
+        pass
+
+    def setup(self):
+        pass
