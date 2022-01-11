513faa2696b3076d85b6adb8b4bbc42ce2dba33b
pganesan <pradeep.ganesan@objectpartners.com>
2022-01-11 15:24:00 -0600

    Added test for linear regression model

--- tests/linear_regression/test_linear_regression_model.py
+++ tests/linear_regression/test_linear_regression_model.py
@@ -1,14 +1,30 @@
+from pytest import mark
+from src.linear_regression_model import LinearRegressionModel
 
-class LinearRegressionFit:
-    model = LinearRegressionWrapperModel(BaseWrapperModel)
+
+
+@mark.smoke
+@mark.linear_regression
+class LinearRegressionTests:
+    model = LinearRegressionModel()
+
+    @mark.fit
     def test_fit(self):
+        data = test_get_data
         assert False
 
+    @mark.evaluate
     def test_evaluate(self):
         assert False
 
+    @mark.predict
     def test_predict(self):
         assert False
 
+    @mark.tune
     def test_tune(self):
+        assert False
+
+    @mark.predict_proba
+    def test_predict_proba(self):
         assert False
