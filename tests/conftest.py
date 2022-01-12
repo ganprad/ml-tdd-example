68f769e7fac1f3749a9919e218860e52193d7dd6
pganesan <pradeep.ganesan@objectpartners.com>
2022-01-12 12:31:14 -0600

    Added config logic.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,20 @@
-from pathlib import Path
-
 import pandas
 import pytest
 
-from validators.data_schema import Constants
+from config import Config
+
+
+def pytest_addoption(parser):
+    parser.addoption("--fn", action="store", help="Data file for testing.")
 
 
 @pytest.fixture(scope="session")
-def get_data_path():
-    return Constants().data_file
+def get_data_path(request):
+    return request.config.getoption("--fn")
+
 
 @pytest.fixture(scope="function")
 def get_dataframe(get_data_path):
-    data = pandas.read_csv(get_data_path)
+    cfg = Config(get_data_path)
+    data = pandas.read_csv(cfg.DATA_PATH)
     return data
