e83479d9dd082b4e9ecd44aece73e976dc3aaff8
pganesan <pradeep.ganesan@objectpartners.com>
2022-01-12 12:30:05 -0600

    WIP data schema.

--- validators/data_schema.py
+++ validators/data_schema.py
@@ -1,27 +1,43 @@
 import os
 from pathlib import Path
-from typing import Literal
 
+from pandera.schemas import DataFrameSchema
 from pydantic import BaseModel, validator
+
 PKG_PATH = Path(__file__).parents[1].resolve()
 DATA_PATH = str(PKG_PATH / "data/data.csv")
 
 
 class Constants(BaseModel):
-    data_file: Literal[DATA_PATH] = DATA_PATH # Check for a data file names data.csv
+    data_file: str = DATA_PATH  # Check for a data file names data.csv
+
+    @validator("data_file")
+    def check_if_data_in_allowed_filenames(cls, value):
+        FILENAMES = ["data", "retrain", "deployment"]
+        if value in FILENAMES:
+            return value
+        else:
+            raise ValueError(f"These are the allowed filenames for the training data: {FILENAMES}")
+
 
 class DataFileValidator(BaseModel):
     data_file: str = Constants().data_file
 
     @validator("data_file")
     def check_if_training_data_exists(cls, value):
-        if os.path.exists(value):
-            return value
+        PKG_PATH = Path(__file__).parents[1].resolve()
+        DATA_PATH = str(PKG_PATH / f"data/{value}.csv")
+        if os.path.exists(DATA_PATH):
+            return DATA_PATH
         else:
             raise ValueError(f"No training data available. Expected data file : {Constants().data_file}")
 
 
-#TODO:
+class InputDataSchemaValidator(DataFrameSchema):
+    # TODO: Perform elementary EDA and build dataframe schema
+    pass
+
+# TODO:
 # Input dataframe schema -> validate the inputs to the application -> preprocess -> Validate preprocessing outputs -> model
 # Input schemas for model params.
 # Input schemas for tune functionality.
