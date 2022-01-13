ca5e3350961de2863ab1ffd6e712b916309c9bb7
Pradeep Ganesan <prad.ganesan@gmail.com>
2022-01-13 09:15:52 -0600

    Added datamodel validation test.

--- tests/linear_regression/test_linear_regression_model.py
+++ tests/linear_regression/test_linear_regression_model.py
@@ -2,7 +2,43 @@ from pytest import mark
 
 from src.linear_regression_model import LinearRegressionModel
 
+from pandera import SchemaModel
+from pydantic import BaseModel, BaseConfig
 
+from validators.data_schema import InputDataFrameSchema, PreprocessedDataSchema, Constants
+from validators.linear_regression_model_schema import (
+    JobParam,
+    HyperParam,
+    HyperC,
+    HyperL1Ratio,
+    HyperMaxIter,
+    ModelParam,
+    OptunaCVParam,
+)
+
+
+def test_if_datamodels_are_valid():
+    assert issubclass(InputDataFrameSchema, SchemaModel)
+    assert issubclass(PreprocessedDataSchema, SchemaModel)
+    assert issubclass(HyperParam, BaseModel)
+    assert issubclass(HyperC, BaseModel)
+    assert issubclass(HyperMaxIter, BaseModel)
+    assert issubclass(HyperL1Ratio, BaseModel)
+    assert issubclass(JobParam, BaseModel)
+    assert issubclass(OptunaCVParam, BaseModel)
+    assert issubclass(ModelParam, BaseModel)
+    assert issubclass(HyperParam.__config__, BaseConfig)
+    assert issubclass(HyperC.__config__, BaseConfig)
+    assert issubclass(HyperMaxIter.__config__, BaseConfig)
+    assert issubclass(HyperL1Ratio.__config__, BaseConfig)
+    assert issubclass(JobParam.__config__, BaseConfig)
+    assert issubclass(Constants, BaseModel)
+    assert issubclass(Constants.__config__, BaseConfig)
+    assert issubclass(OptunaCVParam.__config__, BaseConfig)
+    assert issubclass(ModelParam.__config__, BaseConfig)
+
+
+# TODO:
 @mark.smoke
 @mark.linear_regression
 class LinearRegressionTests:
@@ -28,3 +64,5 @@ class LinearRegressionTests:
     @mark.predict_proba
     def test_predict_proba(self):
         assert False
+
+
